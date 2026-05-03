8b6ed9ac179cae604ff080fc58bb7477dc763076
krerom <133490285+krerom@users.noreply.github.com>
2026-05-03 04:10:23 +0400

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.css"
-export * from "./Buttons/Button";
-export * from "./Layout/Layout";
-export * from "./NavHeader/NavHeader";
-export * from "./Card/Card";
-export * from "./Toast/Toast";
-export * from "./Badge/Badge";
-export * from "./Alert/Alert";
-export * from "./Debounce/DebounceInput";
